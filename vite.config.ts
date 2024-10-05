fe6c817da90e9379191062b02834467dce4701d0
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-10-05 21:17:34 +0400

    configured building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path"
+import dst from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ruben-ui-kit",
+      fileName: format => `ruben-ui-kit.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React"
+        }
+      }
+    }
+  },
+  plugins: [react(),
+    dst({
+      insertTypesEntry: true
+    })
+  ]
 })
